a895eb6e867f15587703f15066da08e86fa0391a
tyfkda <tyfkda@gmail.com>
2016-03-15 13:56:20 +0900

    タブコンポーネントとペインコンポーネントを追加
    `ng-content` を使用

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {MyTabsComponent, MyPaneComponent} from './my-tabs.component';
 
 @Component({
   selector: 'my-app',
+  directives: [MyTabsComponent, MyPaneComponent],
   templateUrl: 'app.component.html',
 })
 
---
+++ src/my-tabs.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-tabs',
+  templateUrl: 'my-tabs.component.html',
+})
+export class MyTabsComponent { }
+
+@Component({
+  selector: 'my-pane',
+  templateUrl: 'my-pane.component.html',
+})
+export class MyPaneComponent { }
