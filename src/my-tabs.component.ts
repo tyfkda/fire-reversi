4c4c595754114f32892d2dcc7ff4ced823fdf417
tyfkda <tyfkda@gmail.com>
2016-03-15 14:01:08 +0900

    ペインのコンストラクタでタブを受け取り、親子関係を設定
    どうやって渡ってくるのか不明…

--- src/my-tabs.component.ts
+++ src/my-tabs.component.ts
@@ -4,10 +4,22 @@ import {Component} from 'angular2/core';
   selector: 'my-tabs',
   templateUrl: 'my-tabs.component.html',
 })
-export class MyTabsComponent { }
+export class MyTabsComponent {
+  constructor() {
+    this.panes = []
+  }
+
+  addPane(pane : MyPaneComponent) {
+    this.panes.push(pane)
+  }
+}
 
 @Component({
   selector: 'my-pane',
   templateUrl: 'my-pane.component.html',
 })
-export class MyPaneComponent { }
+export class MyPaneComponent {
+  constructor(tabs : MyTabsComponent) {
+    tabs.addPane(this)
+  }
+}
