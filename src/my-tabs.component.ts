c306480fa0b16cbeb04a89f3f3a953d1968d7912
tyfkda <tyfkda@gmail.com>
2016-03-15 14:29:23 +0900

    ペインのタイトルを表示し、クリックで選択できるようにする

--- src/my-tabs.component.ts
+++ src/my-tabs.component.ts
@@ -1,8 +1,13 @@
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 @Component({
   selector: 'my-tabs',
   templateUrl: 'my-tabs.component.html',
+  styles:[`
+          .active {
+            background-color: red;
+          }
+          `],
 })
 export class MyTabsComponent {
   constructor() {
@@ -11,6 +16,12 @@ export class MyTabsComponent {
 
   addPane(pane : MyPaneComponent) {
     this.panes.push(pane)
+    pane.selected = this.panes.length == 1
+  }
+
+  onPaneClicked(pane : MyPaneComponent) {
+    console.log(pane)
+    this.panes.forEach(p => p.selected = p == pane)
   }
 }
 
@@ -19,6 +30,8 @@ export class MyTabsComponent {
   templateUrl: 'my-pane.component.html',
 })
 export class MyPaneComponent {
+  @Input() title: string
+
   constructor(tabs : MyTabsComponent) {
     tabs.addPane(this)
   }
