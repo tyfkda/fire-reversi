d643e98fd55e573eed21bab41a9b3348b8287057
tyfkda <tyfkda@gmail.com>
2016-03-19 08:59:46 +0900

    Controlでフォームの入力を監視

--- src/observable.page.ts
+++ src/observable.page.ts
@@ -1,17 +1,29 @@
 import {Component} from 'angular2/core'
+import {Control} from 'angular2/common'
+
 import Rx from 'rxjs/Rx'
 
 @Component({
   template: `
-<h2>Rxjs page</h2>
+<h2>Observable page</h2>
+<input type="text" [ngFormControl]="term"/>
     `,
 })
-export class RxjsPage {
+export class ObservablePage {
+  term = new Control()
+
   constructor() {
     Rx.Observable.of(1, 2, 3)
       .map(x => x + '!!!')
       .subscribe(x => {
         console.log(x)
+      })
+
+    this.term.valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .subscribe(value => {
+        console.log(value)
       })
   }
 }
