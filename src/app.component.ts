0f5f046637b90fe71bae1d2ccfa3d2868ad92023
tyfkda <tyfkda@gmail.com>
2016-03-18 10:30:32 +0900

    タブのテストページ作成

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
+import {TabPage} from './tab.page';
 import {TopPage} from './top.page';
 
 @Component({
@@ -8,6 +9,11 @@ import {TopPage} from './top.page';
   directives: [ROUTER_DIRECTIVES],
   providers: [ROUTER_PROVIDERS],
   template: `
+<nav>
+  <a [routerLink]="['TopPage']">Top</a>
+  <a [routerLink]="['TabPage']">Tab</a>
+</nav>
+
 <h1>My First Angular 2 App</h1>
     <router-outlet></router-outlet>
     `,
@@ -15,9 +21,14 @@ import {TopPage} from './top.page';
 @RouteConfig([
   {
     path: '/',
-    name: 'Top',
+    name: 'TopPage',
     component: TopPage,
     useAsDefault: true,
+  },
+  {
+    path: '/tab',
+    name: 'TabPage',
+    component: TabPage,
   },
 ])
 export class AppComponent {
---
+++ src/tab.page.ts
@@ -0,0 +1,26 @@
+import {Component} from 'angular2/core';
+
+import {MyTabsComponent, MyPaneComponent} from './my-tabs.component';
+
+@Component({
+  selector: 'tab-page',
+  directives: [MyTabsComponent, MyPaneComponent],
+  template: `
+<h2>Tab page</h2>
+
+<my-tabs>
+  <my-pane title="Hello">
+    <h4>Hello</h4>
+    <p>Lorem ipsum dolor sit amet</p>
+  </my-pane>
+  <my-pane title="World">
+    <h4>World</h4>
+    <em>Mauris elementum elementum enim at suscipit.</em>
+    <p><span (click)="count = count + 1">counter: {{count}}</span></p>
+  </my-pane>
+</my-tabs>
+    `,
+})
+export class TabPage {
+  count = 0
+}
