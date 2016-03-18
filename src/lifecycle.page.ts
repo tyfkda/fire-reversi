135703eed4579bec3d37b6085a5291e07c385ef3
tyfkda <tyfkda@gmail.com>
2016-03-18 15:07:39 +0900

    ライフサイクルに関するソースを１つにまとめる

--- src/lifecycle.page.ts
+++ src/lifecycle.page.ts
@@ -1,12 +1,51 @@
-import {Component} from 'angular2/core'
+import {Component, Input, OnInit} from 'angular2/core'
 
-import {ParentComponent} from './parent.component'
+@Component({
+  selector: 'child-component',
+  template: `
+<div style="margin: 4px; border: solid 1px green;">
+    <div>Child: {{name}}</div>
+</div>
+    `,
+})
+export class ChildComponent implements OnInit {
+  @Input() name: string
+
+  ngOnInit() {
+    console.log(`Child[${this.name}]#ngOnInit`)
+  }
+
+  ngOnDestroy() {
+    console.log(`Child[${this.name}]#ngOnDestroy`)
+  }
+}
+
+@Component({
+  selector: 'parent-component',
+  directives: [ChildComponent],
+  template: `
+<div style="margin: 4px; border: solid 1px blue;">
+  <div>Parent</div>
+
+  <child-component name="child1"></child-component>
+  <child-component name="child2"></child-component>
+</div>
+    `,
+})
+export class ParentComponent implements OnInit {
+  ngOnInit() {
+    console.log('Parent#ngOnInit')
+  }
+
+  ngOnDestroy() {
+    console.log('Parent#ngOnDestroy')
+  }
+}
 
 @Component({
   directives: [ParentComponent],
   template: `
 <h2>LifeCycle page</h2>
-
 <parent-component></parent-component>
     `,
 })
