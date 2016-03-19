000ef5aa870dd6f7537f464df2c0ed812cf5db5d
tyfkda <tyfkda@gmail.com>
2016-03-19 10:11:39 +0900

    WikiPediaのサーチを行う
    `ngFormControl` を使ってフォームを `Observable` として扱う。
    
    [Taking advantage of Observables in Angular 2](http://blog.thoughtram.io/angular/2016/01/06/taking-advantage-of-observables-in-angular2.html)

--- src/observable.page.ts
+++ src/observable.page.ts
@@ -1,29 +1,52 @@
-import {Component} from 'angular2/core'
+import {Component, Injectable} from 'angular2/core'
 import {Control} from 'angular2/common'
+import {JSONP_PROVIDERS} from 'angular2/http'
+import {URLSearchParams, Jsonp} from 'angular2/http';
 
 import Rx from 'rxjs/Rx'
 
+@Injectable()
+export class WikipediaService {
+  constructor(private jsonp: Jsonp) {}
+
+  search(term: string) {
+    var search = new URLSearchParams()
+    search.set('action', 'opensearch');
+    search.set('search', term);
+    search.set('format', 'json');
+    return this.jsonp
+      .get('http://en.wikipedia.org/w/api.php?callback=JSONP_CALLBACK', { search })
+      .map((response) => response.json()[1])
+  }
+}
+
 @Component({
+  providers: [JSONP_PROVIDERS, WikipediaService],
   template: `
 <h2>Observable page</h2>
 <input type="text" [ngFormControl]="term"/>
+
+<ul>
+  <li *ngFor="#item of items | async">{{item}}</li>
+</ul>
     `,
 })
 export class ObservablePage {
   term = new Control()
+  items: Rx.Observable<Array<string>>
 
-  constructor() {
+  constructor(private wikipediaService: WikipediaService) {
+    console.log(wikipediaService)
+
     Rx.Observable.of(1, 2, 3)
       .map(x => x + '!!!')
       .subscribe(x => {
         console.log(x)
       })
 
-    this.term.valueChanges
+    this.items = this.term.valueChanges
       .debounceTime(400)
       .distinctUntilChanged()
-      .subscribe(value => {
-        console.log(value)
-      })
+      .switchMap(term => this.wikipediaService.search(term))
   }
 }
