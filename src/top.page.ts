5325fe30fe0555feefb7cc0c6c9213039a569288
tyfkda <tyfkda@gmail.com>
2016-03-19 15:47:32 +0900

    Firebase組み込み

--- src/top.page.ts
+++ src/top.page.ts
@@ -3,7 +3,36 @@ import {Component} from 'angular2/core'
 @Component({
   template: `
 <h2>Top page</h2>
+    <div>
+      <button [hidden]="isLoggedIn" class="twitter" (click)="authWithTwitter()">Sign in with Twitter</button>
+    </div>
+    <div class="message-input">
+      <input [hidden]="!isLoggedIn" #messagetext (keyup)="doneTyping($event)" placeholder="Enter a message...">
+    </div>
     `,
 })
 export class TopPage {
+  firebaseUrl: string
+  messagesRef: Firebase
+  isLoggedIn: boolean;
+  authData: any;
+
+  constructor() {
+    this.firebaseUrl = 'https://2nqujjklgij2gg6v.firebaseio.com/messages'
+    this.messagesRef = new Firebase(this.firebaseUrl)
+    this.messagesRef.onAuth((user) => {
+      console.log(user)
+      if (user) {
+        this.authData = user
+        this.isLoggedIn = true
+      }
+    })
+  }
+
+  authWithTwitter() {
+    this.messagesRef.authWithOAuthPopup('twitter', (error) => {
+      if (error)
+	console.error(error)
+    })
+  }
 }
